30a26a88032a005889a3ed5faf6e520025cc12ae
Efekan.Gvndk <e.gvndk00@gmail.com>
2023-11-09 03:32:12 +0300

    Initial Commit

---
+++ ForDiscovery2.0UITests/ForDiscovery2_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ForDiscovery2_0UITestsLaunchTests.swift
+//  ForDiscovery2.0UITests
+//
+//  Created by Efekan Güvendik on 9.11.2023.
+//
+
+import XCTest
+
+final class ForDiscovery2_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
